874e6d3f0a457c5f5f3f1005d37600f88fc9f573
Khislat Zhuraeva <zhuraevak@gmail.com>
2020-05-08 22:26:56 -0400

    Import function from one file to another

--- my_lambdata/my_mod.py
+++ my_lambdata/my_mod.py
@@ -2,8 +2,14 @@ def enlarge(n):
     return n * 100
     
 
-x = 5 
-print(enlarge(x))
+if __name__ == "__main__": #if we use it the rest of the file my_mode will not be executed
+    #only run the code below if executing this script 
+    # from the command line
+    #otherwise don't run it (for example if we are trying 
+    # to import something)
 
-y = int(input('Please choose a number(e.g. 5): '))
-print(enlarge(y))
+    x = 5 
+    print(enlarge(x))
+
+    y = int(input('Please choose a number(e.g. 5): '))
+    print(enlarge(y))
--- my_lambdata/my_script.py
+++ my_lambdata/my_script.py
@@ -1,5 +1,7 @@
 
 from pandas import DataFrame 
+from my_lambdata.my_mod import enlarge #this works
+
 
 
 print ('Hello')
@@ -7,3 +9,9 @@ print ('Hello')
 df = DataFrame({'a':[1,2,3], 'b':[4,5,6]})
 
 print(df.head())
+
+x = 11 
+print(enlarge(x)) #importing only enlarge function
+#if we use from my_mod import enlarge without "if __name__ == "__main__":"
+# then the entire file my_mod will be executed 
+
