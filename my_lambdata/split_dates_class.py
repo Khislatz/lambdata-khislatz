a49b6d378dc46604382e4ae65d28de4089bc1c0f
Khislat Zhuraeva <zhuraevak@gmail.com>
2020-05-15 19:01:42 -0400

    Working with classes

--- my_lambdata/split_dates_class.py
+++ my_lambdata/split_dates_class.py
@@ -12,9 +12,11 @@ class Dates():
 
     def split_dates(self):
         new_df = self.df.copy()
+
         # Convert dob to datetime
         new_df['dob'] = pd.to_datetime(new_df['dob'],
                                        infer_datetime_format=True)
+
         # Extract components from DOB
         new_df['year'] = new_df['dob'].dt.year                  
         new_df['month'] = new_df['dob'].dt.month
@@ -22,11 +24,12 @@ class Dates():
         print(new_df)
 
 if __name__ == "__main__":
+    
     # Create a dataframe
     df = pd.DataFrame({'Name': ['John', 'Peter', 'Sam', np.nan],
                        'Age': [30, np.nan, np.nan, 45],
                        'Income': [100000, 75000, np.nan, 50000], 
                        'dob': ['01/01/1990', '04/25/1970', '12/19/2000', 
                                '05/02/1975']})
-    df2 = Dates(df)
+    df2 = Dates(df) 
     df2.split_dates()  # inovking the function
