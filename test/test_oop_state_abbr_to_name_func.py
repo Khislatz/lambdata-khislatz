68a1c96d8430952955e95b153f64c5f9e4ce7806
Khislat Zhuraeva <zhuraevak@gmail.com>
2020-05-15 23:49:59 -0400

    Function test oop

--- test/test_oop_state_abbr_to_name_func.py
+++ test/test_oop_state_abbr_to_name_func.py
@@ -1,22 +1,24 @@
 
+
 #OBEJCTIVE: test the add_state_names() function from the my_lambdata/state_abbr_to_name_func.py file  
 # State abbreviation -> Full Name and visa versa. FL -> Florida, etc. 
 # (Handle Washington DC and territories like Puerto Rico etc.)
 from pandas import DataFrame
 import unittest 
-from my_lambdata.state_abbr_to_name_func import add_state_names
+from my_lambdata.state_abbr_to_name_func_oop import DataProcessor
 
-class TestAbbrNames(unittest.TestCase):
+class TestDataProcessor(unittest.TestCase):
         
-    def test_add_state_names(self):
+    def test_add_state_names_oop(self):
         df = DataFrame({"abbrev":["CA", "CT", "CO", "DC", "TX", "FL", "NY"]})
-        self.assertEqual(list(df.columns), ['abbrev'])
-        #todo assert the first row's values are equal to...
-        # breakpoint()
-        result = add_state_names(df)
-        self.assertEqual(list(result.columns), ['abbrev', 'name'])
-        self.assertEqual((result.iloc[0,1]), 'California')
-        self.assertEqual((result.iloc[0]['abbrev']), 'CA')
+        processor = DataProcessor(df)
+        self.assertEqual(list(processor.df.columns), ['abbrev'])
+
+        processor.add_state_names() #processor is mutating the df
+
+        self.assertEqual(list(processor.df.columns), ['abbrev', 'name'])
+        self.assertEqual((processor.df.iloc[0,1]), 'California')
+        self.assertEqual((processor.df.iloc[0]['abbrev']), 'CA')
         # expect that it has one more column and the same number of rows
         # expect that certain column names exist before and 
         # certain column names exist after
